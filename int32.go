b875409699be4b5c957f6191580f61520bde6fab
hslam <791874158@qq.com>
2020-10-08 01:25:43 +0800

    update int32 and uint32

--- int32.go
+++ int32.go
@@ -21,50 +21,25 @@ func NewInt32(val int32) *Int32 {
 
 // Swap atomically stores new into *addr and returns the previous *addr value.
 func (addr *Int32) Swap(new int32) (old int32) {
-	return SwapInt32(addr, new)
+	return atomic.SwapInt32(&addr.v, new)
 }
 
 // CompareAndSwap executes the compare-and-swap operation for an int32 value.
 func (addr *Int32) CompareAndSwap(old, new int32) (swapped bool) {
-	return CompareAndSwapInt32(addr, old, new)
+	return atomic.CompareAndSwapInt32(&addr.v, old, new)
 }
 
 // Add atomically adds delta to *addr and returns the new value.
 func (addr *Int32) Add(delta int32) (new int32) {
-	return AddInt32(addr, delta)
+	return atomic.AddInt32(&addr.v, delta)
 }
 
 // Load atomically loads *addr.
 func (addr *Int32) Load() (val int32) {
-	return LoadInt32(addr)
+	return atomic.LoadInt32(&addr.v)
 }
 
 // Store atomically stores val into *addr.
 func (addr *Int32) Store(val int32) {
-	StoreInt32(addr, val)
-}
-
-// SwapInt32 atomically stores new into *addr and returns the previous *addr value.
-func SwapInt32(addr *Int32, new int32) (old int32) {
-	return atomic.SwapInt32(&addr.v, new)
-}
-
-// CompareAndSwapInt32 executes the compare-and-swap operation for an int32 value.
-func CompareAndSwapInt32(addr *Int32, old, new int32) (swapped bool) {
-	return atomic.CompareAndSwapInt32(&addr.v, old, new)
-}
-
-// AddInt32 atomically adds delta to *addr and returns the new value.
-func AddInt32(addr *Int32, delta int32) (new int32) {
-	return atomic.AddInt32(&addr.v, delta)
-}
-
-// LoadInt32 atomically loads *addr.
-func LoadInt32(addr *Int32) (val int32) {
-	return atomic.LoadInt32(&addr.v)
-}
-
-// StoreInt32 atomically stores val into *addr.
-func StoreInt32(addr *Int32, val int32) {
 	atomic.StoreInt32(&addr.v, val)
 }
--- uint32.go
+++ uint32.go
@@ -21,50 +21,25 @@ func NewUint32(val uint32) *Uint32 {
 
 // Swap atomically stores new into *addr and returns the previous *addr value.
 func (addr *Uint32) Swap(new uint32) (old uint32) {
-	return SwapUint32(addr, new)
+	return atomic.SwapUint32(&addr.v, new)
 }
 
 // CompareAndSwap executes the compare-and-swap operation for an uint32 value.
 func (addr *Uint32) CompareAndSwap(old, new uint32) (swapped bool) {
-	return CompareAndSwapUint32(addr, old, new)
+	return atomic.CompareAndSwapUint32(&addr.v, old, new)
 }
 
 // Add atomically adds delta to *addr and returns the new value.
 func (addr *Uint32) Add(delta uint32) (new uint32) {
-	return AddUint32(addr, delta)
+	return atomic.AddUint32(&addr.v, delta)
 }
 
 // Load atomically loads *addr.
 func (addr *Uint32) Load() (val uint32) {
-	return LoadUint32(addr)
+	return atomic.LoadUint32(&addr.v)
 }
 
 // Store atomically stores val into *addr.
 func (addr *Uint32) Store(val uint32) {
-	StoreUint32(addr, val)
-}
-
-// SwapUint32 atomically stores new into *addr and returns the previous *addr value.
-func SwapUint32(addr *Uint32, new uint32) (old uint32) {
-	return atomic.SwapUint32(&addr.v, new)
-}
-
-// CompareAndSwapUint32 executes the compare-and-swap operation for an uint32 value.
-func CompareAndSwapUint32(addr *Uint32, old, new uint32) (swapped bool) {
-	return atomic.CompareAndSwapUint32(&addr.v, old, new)
-}
-
-// AddUint32 atomically adds delta to *addr and returns the new value.
-func AddUint32(addr *Uint32, delta uint32) (new uint32) {
-	return atomic.AddUint32(&addr.v, delta)
-}
-
-// LoadUint32 atomically loads *addr.
-func LoadUint32(addr *Uint32) (val uint32) {
-	return atomic.LoadUint32(&addr.v)
-}
-
-// StoreUint32 atomically stores val into *addr.
-func StoreUint32(addr *Uint32, val uint32) {
 	atomic.StoreUint32(&addr.v, val)
 }
