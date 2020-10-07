93815123a4f1c6bef97cc395526ffccc459594b4
hslam <791874158@qq.com>
2020-10-08 01:26:40 +0800

    update int64 and uint64

--- int64.go
+++ int64.go
@@ -21,50 +21,25 @@ func NewInt64(val int64) *Int64 {
 
 // Swap atomically stores new into *addr and returns the previous *addr value.
 func (addr *Int64) Swap(new int64) (old int64) {
-	return SwapInt64(addr, new)
+	return atomic.SwapInt64(&addr.v, new)
 }
 
 // CompareAndSwap executes the compare-and-swap operation for an int32 value.
 func (addr *Int64) CompareAndSwap(old, new int64) (swapped bool) {
-	return CompareAndSwapInt64(addr, old, new)
+	return atomic.CompareAndSwapInt64(&addr.v, old, new)
 }
 
 // Add atomically adds delta to *addr and returns the new value.
 func (addr *Int64) Add(delta int64) (new int64) {
-	return AddInt64(addr, delta)
+	return atomic.AddInt64(&addr.v, delta)
 }
 
 // Load atomically loads *addr.
 func (addr *Int64) Load() (val int64) {
-	return LoadInt64(addr)
+	return atomic.LoadInt64(&addr.v)
 }
 
 // Store atomically stores val into *addr.
 func (addr *Int64) Store(val int64) {
-	StoreInt64(addr, val)
-}
-
-// SwapInt64 atomically stores new into *addr and returns the previous *addr value.
-func SwapInt64(addr *Int64, new int64) (old int64) {
-	return atomic.SwapInt64(&addr.v, new)
-}
-
-// CompareAndSwapInt64 executes the compare-and-swap operation for an int32 value.
-func CompareAndSwapInt64(addr *Int64, old, new int64) (swapped bool) {
-	return atomic.CompareAndSwapInt64(&addr.v, old, new)
-}
-
-// AddInt64 atomically adds delta to *addr and returns the new value.
-func AddInt64(addr *Int64, delta int64) (new int64) {
-	return atomic.AddInt64(&addr.v, delta)
-}
-
-// LoadInt64 atomically loads *addr.
-func LoadInt64(addr *Int64) (val int64) {
-	return atomic.LoadInt64(&addr.v)
-}
-
-// StoreInt64 atomically stores val into *addr.
-func StoreInt64(addr *Int64, val int64) {
 	atomic.StoreInt64(&addr.v, val)
 }
--- uint64.go
+++ uint64.go
@@ -21,50 +21,25 @@ func NewUint64(val uint64) *Uint64 {
 
 // Swap atomically stores new into *addr and returns the previous *addr value.
 func (addr *Uint64) Swap(new uint64) (old uint64) {
-	return SwapUint64(addr, new)
+	return atomic.SwapUint64(&addr.v, new)
 }
 
 // CompareAndSwap executes the compare-and-swap operation for an uint64 value.
 func (addr *Uint64) CompareAndSwap(old, new uint64) (swapped bool) {
-	return CompareAndSwapUint64(addr, old, new)
+	return atomic.CompareAndSwapUint64(&addr.v, old, new)
 }
 
 // Add atomically adds delta to *addr and returns the new value.
 func (addr *Uint64) Add(delta uint64) (new uint64) {
-	return AddUint64(addr, delta)
+	return atomic.AddUint64(&addr.v, delta)
 }
 
 // Load atomically loads *addr.
 func (addr *Uint64) Load() (val uint64) {
-	return LoadUint64(addr)
+	return atomic.LoadUint64(&addr.v)
 }
 
 // Store atomically stores val into *addr.
 func (addr *Uint64) Store(val uint64) {
-	StoreUint64(addr, val)
-}
-
-// SwapUint64 atomically stores new into *addr and returns the previous *addr value.
-func SwapUint64(addr *Uint64, new uint64) (old uint64) {
-	return atomic.SwapUint64(&addr.v, new)
-}
-
-// CompareAndSwapUint64 executes the compare-and-swap operation for an uint64 value.
-func CompareAndSwapUint64(addr *Uint64, old, new uint64) (swapped bool) {
-	return atomic.CompareAndSwapUint64(&addr.v, old, new)
-}
-
-// AddUint64 atomically adds delta to *addr and returns the new value.
-func AddUint64(addr *Uint64, delta uint64) (new uint64) {
-	return atomic.AddUint64(&addr.v, delta)
-}
-
-// LoadUint64 atomically loads *addr.
-func LoadUint64(addr *Uint64) (val uint64) {
-	return atomic.LoadUint64(&addr.v)
-}
-
-// StoreUint64 atomically stores val into *addr.
-func StoreUint64(addr *Uint64, val uint64) {
 	atomic.StoreUint64(&addr.v, val)
 }
