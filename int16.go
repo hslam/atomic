0873e3768486abc49ded7b010de37ebee62e3a30
hslam <791874158@qq.com>
2020-10-08 01:24:48 +0800

    update int16 and uint16

--- int16.go
+++ int16.go
@@ -22,50 +22,25 @@ func NewInt16(val int16) *Int16 {
 
 // Swap atomically stores new into *addr and returns the previous *addr value.
 func (addr *Int16) Swap(new int16) (old int16) {
-	return SwapInt16(addr, new)
-}
-
-// CompareAndSwap executes the compare-and-swap operation for an int16 value.
-func (addr *Int16) CompareAndSwap(old, new int16) (swapped bool) {
-	return CompareAndSwapInt16(addr, old, new)
-}
-
-// Add atomically adds delta to *addr and returns the new value.
-func (addr *Int16) Add(delta int16) (new int16) {
-	return AddInt16(addr, delta)
-}
-
-// Load atomically loads *addr.
-func (addr *Int16) Load() (val int16) {
-	return LoadInt16(addr)
-}
-
-// Store atomically stores val into *addr.
-func (addr *Int16) Store(val int16) {
-	StoreInt16(addr, val)
-}
-
-// SwapInt16 atomically stores new into *addr and returns the previous *addr value.
-func SwapInt16(addr *Int16, new int16) (old int16) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		old = LoadInt16(addr)
-		StoreInt16(addr, new)
+		old = addr.Load()
+		addr.Store(new)
 		atomic.StoreUint32(&addr.seting, 0)
 		return
 	}
 }
 
-// CompareAndSwapInt16 executes the compare-and-swap operation for an int16 value.
-func CompareAndSwapInt16(addr *Int16, old, new int16) (swapped bool) {
+// CompareAndSwap executes the compare-and-swap operation for an int16 value.
+func (addr *Int16) CompareAndSwap(old, new int16) (swapped bool) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		if LoadInt16(addr) == old {
-			StoreInt16(addr, new)
+		if addr.Load() == old {
+			addr.Store(new)
 			atomic.StoreUint32(&addr.seting, 0)
 			return true
 		}
@@ -74,26 +49,26 @@ func CompareAndSwapInt16(addr *Int16, old, new int16) (swapped bool) {
 	}
 }
 
-// AddInt16 atomically adds delta to *addr and returns the new value.
-func AddInt16(addr *Int16, delta int16) (new int16) {
+// Add atomically adds delta to *addr and returns the new value.
+func (addr *Int16) Add(delta int16) (new int16) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		new = LoadInt16(addr) + delta
-		StoreInt16(addr, new)
+		new = addr.Load() + delta
+		addr.Store(new)
 		atomic.StoreUint32(&addr.seting, 0)
 		return
 	}
 }
 
-// LoadInt16 atomically loads *addr.
-func LoadInt16(addr *Int16) (val int16) {
+// Load atomically loads *addr.
+func (addr *Int16) Load() (val int16) {
 	var v = atomic.LoadUint32(&addr.v)
 	return int16(v)
 }
 
-// StoreInt16 atomically stores val into *addr.
-func StoreInt16(addr *Int16, val int16) {
+// Store atomically stores val into *addr.
+func (addr *Int16) Store(val int16) {
 	atomic.StoreUint32(&addr.v, uint32(val))
 }
--- uint16.go
+++ uint16.go
@@ -22,50 +22,25 @@ func NewUint16(val uint16) *Uint16 {
 
 // Swap atomically stores new into *addr and returns the previous *addr value.
 func (addr *Uint16) Swap(new uint16) (old uint16) {
-	return SwapUint16(addr, new)
-}
-
-// CompareAndSwap executes the compare-and-swap operation for an uint16 value.
-func (addr *Uint16) CompareAndSwap(old, new uint16) (swapped bool) {
-	return CompareAndSwapUint16(addr, old, new)
-}
-
-// Add atomically adds delta to *addr and returns the new value.
-func (addr *Uint16) Add(delta uint16) (new uint16) {
-	return AddUint16(addr, delta)
-}
-
-// Load atomically loads *addr.
-func (addr *Uint16) Load() (val uint16) {
-	return LoadUint16(addr)
-}
-
-// Store atomically stores val into *addr.
-func (addr *Uint16) Store(val uint16) {
-	StoreUint16(addr, val)
-}
-
-// SwapUint16 atomically stores new into *addr and returns the previous *addr value.
-func SwapUint16(addr *Uint16, new uint16) (old uint16) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		old = LoadUint16(addr)
-		StoreUint16(addr, new)
+		old = addr.Load()
+		addr.Store(new)
 		atomic.StoreUint32(&addr.seting, 0)
 		return
 	}
 }
 
-// CompareAndSwapUint16 executes the compare-and-swap operation for an uint16 value.
-func CompareAndSwapUint16(addr *Uint16, old, new uint16) (swapped bool) {
+// CompareAndSwap executes the compare-and-swap operation for an uint16 value.
+func (addr *Uint16) CompareAndSwap(old, new uint16) (swapped bool) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		if LoadUint16(addr) == old {
-			StoreUint16(addr, new)
+		if addr.Load() == old {
+			addr.Store(new)
 			atomic.StoreUint32(&addr.seting, 0)
 			return true
 		}
@@ -74,26 +49,26 @@ func CompareAndSwapUint16(addr *Uint16, old, new uint16) (swapped bool) {
 	}
 }
 
-// AddUint16 atomically adds delta to *addr and returns the new value.
-func AddUint16(addr *Uint16, delta uint16) (new uint16) {
+// Add atomically adds delta to *addr and returns the new value.
+func (addr *Uint16) Add(delta uint16) (new uint16) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		new = LoadUint16(addr) + delta
-		StoreUint16(addr, new)
+		new = addr.Load() + delta
+		addr.Store(new)
 		atomic.StoreUint32(&addr.seting, 0)
 		return
 	}
 }
 
-// LoadUint16 atomically loads *addr.
-func LoadUint16(addr *Uint16) (val uint16) {
+// Load atomically loads *addr.
+func (addr *Uint16) Load() (val uint16) {
 	var v = atomic.LoadUint32(&addr.v)
 	return uint16(v)
 }
 
-// StoreUint16 atomically stores val into *addr.
-func StoreUint16(addr *Uint16, val uint16) {
+// Store atomically stores val into *addr.
+func (addr *Uint16) Store(val uint16) {
 	atomic.StoreUint32(&addr.v, uint32(val))
 }
