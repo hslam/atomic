5ccf4d7fe865ea7765a2f7086db5863cbf277b85
hslam <791874158@qq.com>
2020-10-08 01:21:51 +0800

    update float32 and float64

--- float32.go
+++ float32.go
@@ -23,50 +23,25 @@ func NewFloat32(val float32) *Float32 {
 
 // Swap atomically stores new into *addr and returns the previous *addr value.
 func (addr *Float32) Swap(new float32) (old float32) {
-	return SwapFloat32(addr, new)
-}
-
-// CompareAndSwap executes the compare-and-swap operation for an float32 value.
-func (addr *Float32) CompareAndSwap(old, new float32) (swapped bool) {
-	return CompareAndSwapFloat32(addr, old, new)
-}
-
-// Add atomically adds delta to *addr and returns the new value.
-func (addr *Float32) Add(delta float32) (new float32) {
-	return AddFloat32(addr, delta)
-}
-
-// Load atomically loads *addr.
-func (addr *Float32) Load() (val float32) {
-	return LoadFloat32(addr)
-}
-
-// Store atomically stores val into *addr.
-func (addr *Float32) Store(val float32) {
-	StoreFloat32(addr, val)
-}
-
-// SwapFloat32 atomically stores new into *addr and returns the previous *addr value.
-func SwapFloat32(addr *Float32, new float32) (old float32) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		old = LoadFloat32(addr)
-		StoreFloat32(addr, new)
+		old = addr.Load()
+		addr.Store(new)
 		atomic.StoreUint32(&addr.seting, 0)
 		return
 	}
 }
 
-// CompareAndSwapFloat32 executes the compare-and-swap operation for an float32 value.
-func CompareAndSwapFloat32(addr *Float32, old, new float32) (swapped bool) {
+// CompareAndSwap executes the compare-and-swap operation for an float32 value.
+func (addr *Float32) CompareAndSwap(old, new float32) (swapped bool) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		if LoadFloat32(addr) == old {
-			StoreFloat32(addr, new)
+		if addr.Load() == old {
+			addr.Store(new)
 			atomic.StoreUint32(&addr.seting, 0)
 			return true
 		}
@@ -75,26 +50,26 @@ func CompareAndSwapFloat32(addr *Float32, old, new float32) (swapped bool) {
 	}
 }
 
-// AddFloat32 atomically adds delta to *addr and returns the new value.
-func AddFloat32(addr *Float32, delta float32) (new float32) {
+// Add atomically adds delta to *addr and returns the new value.
+func (addr *Float32) Add(delta float32) (new float32) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		new = LoadFloat32(addr) + delta
-		StoreFloat32(addr, new)
+		new = addr.Load() + delta
+		addr.Store(new)
 		atomic.StoreUint32(&addr.seting, 0)
 		return
 	}
 }
 
-// LoadFloat32 atomically loads *addr.
-func LoadFloat32(addr *Float32) (val float32) {
+// Load atomically loads *addr.
+func (addr *Float32) Load() (val float32) {
 	var v = atomic.LoadUint32(&addr.v)
 	return *(*float32)(unsafe.Pointer(&v))
 }
 
-// StoreFloat32 atomically stores val into *addr.
-func StoreFloat32(addr *Float32, val float32) {
+// Store atomically stores val into *addr.
+func (addr *Float32) Store(val float32) {
 	atomic.StoreUint32(&addr.v, *(*uint32)(unsafe.Pointer(&val)))
 }
--- float64.go
+++ float64.go
@@ -23,50 +23,25 @@ func NewFloat64(val float64) *Float64 {
 
 // Swap atomically stores new into *addr and returns the previous *addr value.
 func (addr *Float64) Swap(new float64) (old float64) {
-	return SwapFloat64(addr, new)
-}
-
-// CompareAndSwap executes the compare-and-swap operation for an float64 value.
-func (addr *Float64) CompareAndSwap(old, new float64) (swapped bool) {
-	return CompareAndSwapFloat64(addr, old, new)
-}
-
-// Add atomically adds delta to *addr and returns the new value.
-func (addr *Float64) Add(delta float64) (new float64) {
-	return AddFloat64(addr, delta)
-}
-
-// Load atomically loads *addr.
-func (addr *Float64) Load() (val float64) {
-	return LoadFloat64(addr)
-}
-
-// Store atomically stores val into *addr.
-func (addr *Float64) Store(val float64) {
-	StoreFloat64(addr, val)
-}
-
-// SwapFloat64 atomically stores new into *addr and returns the previous *addr value.
-func SwapFloat64(addr *Float64, new float64) (old float64) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		old = LoadFloat64(addr)
-		StoreFloat64(addr, new)
+		old = addr.Load()
+		addr.Store(new)
 		atomic.StoreUint32(&addr.seting, 0)
 		return
 	}
 }
 
-// CompareAndSwapFloat64 executes the compare-and-swap operation for an float64 value.
-func CompareAndSwapFloat64(addr *Float64, old, new float64) (swapped bool) {
+// CompareAndSwap executes the compare-and-swap operation for an float64 value.
+func (addr *Float64) CompareAndSwap(old, new float64) (swapped bool) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		if LoadFloat64(addr) == old {
-			StoreFloat64(addr, new)
+		if addr.Load() == old {
+			addr.Store(new)
 			atomic.StoreUint32(&addr.seting, 0)
 			return true
 		}
@@ -75,26 +50,26 @@ func CompareAndSwapFloat64(addr *Float64, old, new float64) (swapped bool) {
 	}
 }
 
-// AddFloat64 atomically adds delta to *addr and returns the new value.
-func AddFloat64(addr *Float64, delta float64) (new float64) {
+// Add atomically adds delta to *addr and returns the new value.
+func (addr *Float64) Add(delta float64) (new float64) {
 	for {
 		if !atomic.CompareAndSwapUint32(&addr.seting, 0, 1) {
 			continue
 		}
-		new = LoadFloat64(addr) + delta
-		StoreFloat64(addr, new)
+		new = addr.Load() + delta
+		addr.Store(new)
 		atomic.StoreUint32(&addr.seting, 0)
 		return
 	}
 }
 
-// LoadFloat64 atomically loads *addr.
-func LoadFloat64(addr *Float64) (val float64) {
+// Load atomically loads *addr.
+func (addr *Float64) Load() (val float64) {
 	var v = atomic.LoadUint64(&addr.v)
 	return *(*float64)(unsafe.Pointer(&v))
 }
 
-// StoreFloat64 atomically stores val into *addr.
-func StoreFloat64(addr *Float64, val float64) {
+// Store atomically stores val into *addr.
+func (addr *Float64) Store(val float64) {
 	atomic.StoreUint64(&addr.v, *(*uint64)(unsafe.Pointer(&val)))
 }
