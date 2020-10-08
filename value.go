5b693ce546367f98d4a215ab834d8cd5d7ef26af
hslam <791874158@qq.com>
2020-10-08 12:01:30 +0800

    update value

--- value.go
+++ value.go
@@ -2,21 +2,71 @@ package atomic
 
 import "sync/atomic"
 
+// AddFunc is a add function.
+type AddFunc func(old, delta interface{}) (new interface{})
+
 // Value provides an atomic load and store of a consistently typed value.
 // The zero value for a Value returns nil from Load.
 // Once Store has been called, a Value must not be copied.
 //
 // A Value must not be copied after first use.
 type Value struct {
-	v      *atomic.Value
-	inited uint32
+	v       *atomic.Value
+	inited  uint32
+	seting  uint32
+	AddFunc AddFunc
 }
 
 // NewValue returns a new Value.
-func NewValue(val string) *Value {
-	addr := &Value{v: &atomic.Value{}}
+func NewValue(val interface{}, addFunc AddFunc) *Value {
+	addr := &Value{v: &atomic.Value{}, AddFunc: addFunc}
 	addr.Store(val)
 	return addr
+}
+
+// Swap atomically stores new into *addr and returns the previous *addr value.
+func (v *Value) Swap(new interface{}) (old interface{}) {
+	for {
+		if !atomic.CompareAndSwapUint32(&v.seting, 0, 1) {
+			continue
+		}
+		old = v.Load()
+		v.Store(new)
+		atomic.StoreUint32(&v.seting, 0)
+		return
+	}
+}
+
+// CompareAndSwap executes the compare-and-swap operation for an interface{} value.
+func (v *Value) CompareAndSwap(old, new interface{}) (swapped bool) {
+	for {
+		if !atomic.CompareAndSwapUint32(&v.seting, 0, 1) {
+			continue
+		}
+		if v.Load() == old {
+			v.Store(new)
+			atomic.StoreUint32(&v.seting, 0)
+			return true
+		}
+		atomic.StoreUint32(&v.seting, 0)
+		return false
+	}
+}
+
+// Add atomically adds delta to *addr and returns the new value.
+func (v *Value) Add(delta interface{}) (new interface{}) {
+	if v.AddFunc == nil {
+		panic("AddFunc is nil")
+	}
+	for {
+		if !atomic.CompareAndSwapUint32(&v.seting, 0, 1) {
+			continue
+		}
+		new = v.AddFunc(v.Load(), delta)
+		v.Store(new)
+		atomic.StoreUint32(&v.seting, 0)
+		return
+	}
 }
 
 // Load returns the value set by the most recent Store.
--- value_test.go
+++ value_test.go
@@ -7,7 +7,10 @@ import (
 
 func TestValue(t *testing.T) {
 	var val = "Hello World"
-	addr := NewValue(val)
+	var addFunc AddFunc = func(old, delta interface{}) (new interface{}) {
+		return old.(string) + delta.(string)
+	}
+	addr := NewValue(val, addFunc)
 	if addr.Load() != val {
 		t.Error(addr.Load())
 	}
@@ -19,6 +22,81 @@ func TestValue(t *testing.T) {
 	if addr.Load().(string) != val[:5] {
 		t.Error(addr.Load())
 	}
+	var delta = val[5:]
+	if addr.Add(delta) != val {
+		t.Error(addr.Load())
+	}
+	if addr.Load() != val {
+		t.Error(addr.Load())
+	}
+	var new = "Foo"
+	if addr.Swap(new) != val {
+		t.Error(addr.Load())
+	}
+	var old = new
+	new = "Bar"
+	if !addr.CompareAndSwap(old, new) {
+		t.Error(addr.Load())
+	}
+	if addr.CompareAndSwap(old, new) {
+		t.Error(addr.Load())
+	}
+
+	addr = &Value{}
+	if addr.Load() != nil {
+		t.Error(addr.Load())
+	}
+}
+
+func TestAddValue(t *testing.T) {
+	var addFunc AddFunc = func(old, delta interface{}) (new interface{}) {
+		return old.(string) + delta.(string)
+	}
+	addr := NewValue("", nil)
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				t.Error("should panic")
+			}
+		}()
+		addr.Add("")
+	}()
+	addr.AddFunc = addFunc
+	var wg sync.WaitGroup
+	for i := 0; i < 512; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			addr.Add("")
+		}()
+	}
+	wg.Wait()
+}
+
+func TestCompareAndSwapValue(t *testing.T) {
+	addr := NewValue("", nil)
+	var wg sync.WaitGroup
+	for i := 0; i < 512; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			addr.CompareAndSwap("", "")
+		}()
+	}
+	wg.Wait()
+}
+
+func TestSwapValue(t *testing.T) {
+	addr := NewValue("", nil)
+	var wg sync.WaitGroup
+	for i := 0; i < 512; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			addr.Swap("")
+		}()
+	}
+	wg.Wait()
 }
 
 func TestInitValue(t *testing.T) {
