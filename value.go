46de11da42acb0ff20e0c77fca47504e7e82a94c
hslam <791874158@qq.com>
2020-10-10 02:38:42 +0800

    add fast value

--- value.go
+++ value.go
@@ -1,6 +1,9 @@
 package atomic
 
-import "sync/atomic"
+import (
+	"sync/atomic"
+	"unsafe"
+)
 
 // AddFunc is a add function.
 type AddFunc func(old, delta interface{}) (new interface{})
@@ -11,9 +14,16 @@ type AddFunc func(old, delta interface{}) (new interface{})
 //
 // A Value must not be copied after first use.
 type Value struct {
+	fast    bool
 	v       atomic.Value
 	seting  uint32
 	AddFunc AddFunc
+}
+
+// ifaceWords is interface{} internal representation.
+type ifaceWords struct {
+	typ  unsafe.Pointer
+	data unsafe.Pointer
 }
 
 // NewValue returns a new Value.
@@ -23,21 +33,66 @@ func NewValue(val interface{}, addFunc AddFunc) *Value {
 	return addr
 }
 
+// NewFastValue returns a new FastValue.
+func NewFastValue(val interface{}, addFunc AddFunc) *Value {
+	addr := &Value{AddFunc: addFunc, fast: true}
+	addr.Store(val)
+	return addr
+}
+
 // Swap atomically stores new into *addr and returns the previous *addr value.
 func (v *Value) Swap(new interface{}) (old interface{}) {
 	for {
-		if !atomic.CompareAndSwapUint32(&v.seting, 0, 1) {
-			continue
-		}
 		old = v.Load()
-		v.Store(new)
-		atomic.StoreUint32(&v.seting, 0)
-		return
+		if v.CompareAndSwap(old, new) {
+			return
+		}
 	}
 }
 
 // CompareAndSwap executes the compare-and-swap operation for an interface{} value.
 func (v *Value) CompareAndSwap(old, new interface{}) (swapped bool) {
+	if v.fast {
+		if new == nil {
+			panic("github.com/hslam/atomic: new is nil")
+		}
+		vp := (*ifaceWords)(unsafe.Pointer(&v.v))
+		np := (*ifaceWords)(unsafe.Pointer(&new))
+		for {
+			typ := LoadPointer(&vp.typ)
+			if typ == nil {
+				// Attempt to start first store.
+				// Disable preemption so that other goroutines can use
+				// active spin wait to wait for completion; and so that
+				// GC does not see the fake type accidentally.
+				if !CompareAndSwapPointer(&vp.typ, nil, unsafe.Pointer(^uintptr(0))) {
+					return false
+				}
+				// Complete first store.
+				StorePointer(&vp.data, np.data)
+				StorePointer(&vp.typ, np.typ)
+				return
+			}
+			if uintptr(typ) == ^uintptr(0) {
+				// First store in progress. Wait.
+				// Since we disable preemption around the first store,
+				// we can wait with active spinning.
+				return false
+			}
+			if old == nil {
+				panic("github.com/hslam/atomic: old is nil")
+			}
+			// First store completed. Check type and overwrite data.
+			op := (*ifaceWords)(unsafe.Pointer(&old))
+			if typ != op.typ {
+				panic("github.com/hslam/atomic: old is inconsistently typed value")
+			}
+			if typ != np.typ {
+				panic("github.com/hslam/atomic: new is inconsistently typed value")
+			}
+			return atomic.CompareAndSwapPointer(&vp.data, op.data, np.data)
+		}
+	}
 	for {
 		if !atomic.CompareAndSwapUint32(&v.seting, 0, 1) {
 			continue
@@ -58,13 +113,11 @@ func (v *Value) Add(delta interface{}) (new interface{}) {
 		panic("AddFunc is nil")
 	}
 	for {
-		if !atomic.CompareAndSwapUint32(&v.seting, 0, 1) {
-			continue
+		old := v.Load()
+		new = v.AddFunc(old, delta)
+		if v.CompareAndSwap(old, new) {
+			return
 		}
-		new = v.AddFunc(v.Load(), delta)
-		v.Store(new)
-		atomic.StoreUint32(&v.seting, 0)
-		return
 	}
 }
 
--- value_test.go
+++ value_test.go
@@ -37,7 +37,6 @@ func TestValue(t *testing.T) {
 	if addr.CompareAndSwap(old, new) {
 		t.Error(addr.Load())
 	}
-
 	addr = &Value{}
 	if addr.Load() != nil {
 		t.Error(addr.Load())
@@ -128,6 +127,173 @@ func BenchmarkStoreValue(b *testing.B) {
 
 func BenchmarkLoadValue(b *testing.B) {
 	addr := NewValue("", nil)
+	for i := 0; i < b.N; i++ {
+		addr.Load()
+	}
+}
+
+func TestFastValue(t *testing.T) {
+	var val = "Hello World"
+	var addFunc AddFunc = func(old, delta interface{}) (new interface{}) {
+		return old.(string) + delta.(string)
+	}
+	addr := NewFastValue(val, addFunc)
+	if addr.Load() != val {
+		t.Error(addr.Load())
+	}
+	addr.Store(val[:5])
+	if addr.Load().(string) != val[:5] {
+		t.Error(addr.Load())
+	}
+	var delta = val[5:]
+	if addr.Add(delta) != val {
+		t.Error(addr.Load())
+	}
+	if addr.Load() != val {
+		t.Error(addr.Load())
+	}
+	var new = "Foo"
+	if addr.Swap(new) != val {
+		t.Error(addr.Load())
+	}
+	var old = addr.Load()
+	new = "Bar"
+	if !addr.CompareAndSwap(old, new) {
+		t.Error(addr.Load())
+	}
+	if addr.CompareAndSwap(old, new) {
+		t.Error(addr.Load())
+	}
+	addr = &Value{fast: true}
+	if addr.Load() != nil {
+		t.Error(addr.Load())
+	}
+}
+
+func TestAddFastValue(t *testing.T) {
+	var addFunc AddFunc = func(old, delta interface{}) (new interface{}) {
+		return old.(string) + delta.(string)
+	}
+	addr := NewFastValue("", nil)
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				t.Error("should panic")
+			}
+		}()
+		addr.Add("")
+	}()
+	addr.AddFunc = addFunc
+	var wg sync.WaitGroup
+	for i := 0; i < 16382; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			addr.Add("")
+		}()
+	}
+	wg.Wait()
+}
+
+func testCompareAndSwapFastValue(t *testing.T) {
+	addr := &Value{fast: true}
+	var wg sync.WaitGroup
+	for i := 0; i < 64; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			addr.CompareAndSwap("", "")
+		}()
+	}
+	wg.Wait()
+}
+
+func TestCompareAndSwapFastValue(t *testing.T) {
+	for i := 0; i < 16382; i++ {
+		testCompareAndSwapFastValue(t)
+	}
+	addr := &Value{fast: true}
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				t.Error("should panic")
+			}
+		}()
+		addr.CompareAndSwap("", nil)
+	}()
+	addr.CompareAndSwap("", "")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				t.Error("should panic")
+			}
+		}()
+		addr.CompareAndSwap(nil, "")
+	}()
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				t.Error("should panic")
+			}
+		}()
+		addr.CompareAndSwap(1, "")
+	}()
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				t.Error("should panic")
+			}
+		}()
+		addr.CompareAndSwap("", 1)
+	}()
+}
+
+func TestSwapFastValue(t *testing.T) {
+	addr := NewValue("", nil)
+	var wg sync.WaitGroup
+	for i := 0; i < 16382; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			addr.Swap("")
+		}()
+	}
+	wg.Wait()
+}
+
+func BenchmarkSwapFastValue(b *testing.B) {
+	addr := NewFastValue("", nil)
+	for i := 0; i < b.N; i++ {
+		addr.Swap("")
+	}
+}
+
+func BenchmarkCompareAndSwapFastValue(b *testing.B) {
+	addr := NewFastValue("", nil)
+	for i := 0; i < b.N; i++ {
+		addr.CompareAndSwap("", "")
+	}
+}
+
+func BenchmarkAddFastValue(b *testing.B) {
+	var addFunc AddFunc = func(old, delta interface{}) (new interface{}) {
+		return old.(string) + delta.(string)
+	}
+	addr := NewFastValue("", addFunc)
+	for i := 0; i < b.N; i++ {
+		addr.Add("")
+	}
+}
+
+func BenchmarkStoreFastValue(b *testing.B) {
+	addr := NewFastValue("", nil)
+	for i := 0; i < b.N; i++ {
+		addr.Store("")
+	}
+}
+
+func BenchmarkLoadFastValue(b *testing.B) {
+	addr := NewFastValue("", nil)
 	for i := 0; i < b.N; i++ {
 		addr.Load()
 	}
